86de644d042e3b5f42e0472cd51a8045418a6dc3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-06-21 15:34:47 -0400

    Bacon ipsum 🐖

---
+++ positioning/loremIpsum.js
@@ -0,0 +1,5 @@
+// Bacon ipsum
+// http://fabricjs.com/test/misc/itext.html
+function loremIpsum() {
+  document.writeln(`Bacon ipsum dolor sit amet beef ribs flank pancetta kevin biltong, pork doner andouille ball tip ribeye pork loin short loin boudin. Fatback leberkas ball tip andouille shank ribeye spare ribs, venison pastrami ground round bacon short loin beef ribs tri-tip biltong. Fatback spare ribs flank chuck short ribs salami, ground round biltong leberkas bresaola ribeye filet mignon. Swine kevin meatloaf turducken, drumstick boudin pancetta tongue ham filet mignon ham hock salami. Beef ribs sausage ribeye spare ribs kevin frankfurter. Meatball corned beef biltong sirloin turducken jerky. Flank filet mignon prosciutto, ham hock pork fatback spare ribs capicola jerky.`);
+}
