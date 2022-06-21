8a35e1effe666b51c8d8fc58cbaa0dbceed793ad
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-06-21 15:33:56 -0400

    Delete loremIpsumBullshit.js

--- positioning/loremIpsumBullshit.js
+++
@@ -1,5 +0,0 @@
-function loremIpsumBullshit() {
-  document.writeln(`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur mollitia maxime facere quae cumque perferendis cum atque quia repellendus rerum eaque quod quibusdam incidunt blanditiis possimus temporibus reiciendis deserunt sequi eveniet necessitatibus
-maiores quas assumenda voluptate qui odio laboriosam totam repudiandae? Doloremque dignissimos voluptatibus eveniet rem quasi minus ex cumque esse culpa cupiditate cum architecto! Facilis deleniti unde suscipit minima obcaecati vero ea soluta odio cupiditate
-placeat vitae nesciunt quis alias dolorum nemo sint facere. Deleniti itaque incidunt eligendi qui nemo corporis ducimus beatae consequatur est iusto dolorum consequuntur vero debitis saepe voluptatem impedit sint ea numquam quia voluptate quidem.`)
-}
